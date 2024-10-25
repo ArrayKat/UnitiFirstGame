8239cb4b3606b2109116ad8a3f4631b9a41728ec
ArrayKat <kolinichenko.05@mail.ru>
2024-10-25 23:46:28 +0300

    add lesson 8

--- Assets/Shoot.cs
+++ Assets/Shoot.cs
@@ -7,11 +7,14 @@ public class Shoot : MonoBehaviour
     public GameObject arrowPrefab;
     public Transform arrowLocation;
     public float shotPower;
+    public AudioSource shootSound;
+
 
     void Update()
     {
         if (Input.GetButtonDown("Fire1")) {
             Instantiate(arrowPrefab, arrowLocation.position, arrowLocation.rotation).GetComponent<Rigidbody>().AddRelativeForce(Vector3.forward * shotPower);
+            shootSound.Play();
         }
     }
 }

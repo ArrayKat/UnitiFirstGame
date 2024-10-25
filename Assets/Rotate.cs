25074dbc9b174660a6a2b287a9f6c53ac55277ed
ArrayKat <kolinichenko.05@mail.ru>
2024-10-26 00:58:11 +0300

    add home work

--- Assets/Rotate.cs
+++ Assets/Rotate.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class Rotate : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0, 1, 0);
+        transform.Rotate(0, 0, 1);
     }
 }

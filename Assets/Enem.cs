aff7f8418e13434661bfc7efec4c6a0d7962dc65
ArrayKat <kolinichenko.05@mail.ru>
2024-10-25 23:08:45 +0300

    add lesson 6

---
+++ Assets/Enem.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.AI;
+
+public class Enem : MonoBehaviour
+{
+    public Slider slider; //полоска здоья
+    public GameObject player; //объект нашего игрока
+    NavMeshAgent agent; //объект бандита
+    Transform target; //цель бандита
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        target = player.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(target.position);
+        if ((player.transform.position - agent.transform.position).sqrMagnitude < 10) slider.value -= 10 * Time.deltaTime;
+    }
+}
